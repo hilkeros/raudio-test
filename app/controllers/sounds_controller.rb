cf4d623695bbfae7deb20c65bddaf8500e68fa19
Hilke Ros <hilkeros@gmail.com>
2017-12-03 17:43:39 +0100

    beat loop

--- app/controllers/sounds_controller.rb
+++ app/controllers/sounds_controller.rb
@@ -21,10 +21,26 @@ class SoundsController < ApplicationController
   		'E1': '/samples/lex/gick.wav'
   		)
   	@sampler.to_master
-  	@button1 = NxButton.new(event: @sampler.trigger_attack('C1'))
-  	@button2 = NxButton.new(event: @sampler.trigger_attack('D1'))
-  	@button3 = NxButton.new(event: @sampler.trigger_attack('E1'))
+  	@button1 = NxButton.new(@sampler.trigger_attack('C1'))
+  	@button2 = NxButton.new(@sampler.trigger_attack('D1'))
+  	@button3 = NxButton.new(@sampler.trigger_attack('E1'))
   	@raudio = Audio.new.render(@sampler)
+  	@nexus = Nexus.new.render(@button1, @button2, @button3)
+  end
+
+  def beat_loop
+  	@sampler = Sampler.new(
+  		'C1': '/samples/lex/clap.wav',
+  		'D1': '/samples/lex/snare.wav',
+  		'E1': '/samples/lex/gick.wav'
+  		)
+  	@sampler.to_master
+  	@clap_loop = Loop.new(@sampler.trigger_attack('C1', '4n'), '2n')
+  	@snare_loop = Loop.new(@sampler.trigger_attack('D1'), '8n')
+  	@button1 = NxButton.new(@clap_loop.start)
+  	@button2 = NxButton.new(@snare_loop.start)
+  	@button3 = NxButton.new(@clap_loop.stop, @snare_loop.stop)
+  	@raudio = Audio.new.render(@sampler, @clap_loop, @snare_loop)
   	@nexus = Nexus.new.render(@button1, @button2, @button3)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get 'sounds/sine'
-
   get 'sounds/sampler'
+  get 'sounds/beat_loop'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
