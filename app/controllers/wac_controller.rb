32bc8072c46f8435fdaf7af78cbde554efddf7a0
Hilke Ros <hilke@mooris.ch>
2018-09-17 16:59:17 +0200

    static effects

--- app/controllers/wac_controller.rb
+++ app/controllers/wac_controller.rb
@@ -49,24 +49,26 @@ class WacController < ApplicationController
 	end
 
 	def effects
-		ableton = Ableton.new('wac.als')
+		ableton = Ableton.new('wacs.als')
     beats = ableton.midi_tracks[0].drum_rack.build_sampler('/samples/wac/')
    	samples = ableton.midi_tracks[1].drum_rack.build_sampler('/samples/wac/')
 
    	distortion = Distortion.new
    	beats.connect(distortion)
-   	disortion.to_master
+   	distortion.to_master
 
    	delay = FeedbackDelay.new('8n', 0.5)
   	samples.connect(delay)
   	delay.to_master
 
   	parts, tracks, scenes = ableton.build_session_for_instruments(beats, samples)
-    @raudio = Audio.new(bpm: 80).render(beats, samples, distortion, delay, *parts, *tracks, *scenes)
+    @raudio = Audio.new(bpm: 80).render(distortion, beats, delay, samples, *parts, *tracks, *scenes)
 
-    increase_delay = ScrollY.new(y, '>100', delay('delayTime': y/100))
-    increase_distortion = ScrollX.new(x, '>500', distortion('wet': (x - 500)/100))
-    @interaction = Interaction.new.render(increase_delay, increase_distortion)
+    # increase_delay = ScrollY.new(y, '>100', delay('delayTime': y/100))
+    # increase_distortion = ScrollX.new(x, '>500', distortion('wet': (x - 500)/100))
+    # @interaction = Interaction.new.render(increase_delay, increase_distortion)
+
+    @nexus = Nexus.new.render(@start_scene_1 = NxButton.new(scenes[0].start_scene))
   	
 	end
 
