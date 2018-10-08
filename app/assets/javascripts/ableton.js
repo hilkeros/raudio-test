e786938177d2f8e7743f3914e4ec1471e056ebbc
Hilke Ros <hilke@mooris.ch>
2018-10-08 19:33:45 +0200

    shorter javascript function for to tone values

--- app/assets/javascripts/ableton.js
+++ app/assets/javascripts/ableton.js
@@ -10,23 +10,10 @@ function start_in_session(part, track) {
 
 function start_scene(scene) {
 	scene.forEach(function (part) {
-		//should be start_in_session, but for that we need to know to which track the part belongs
 		start_in_session(part, part.track); 
 	})
 }
 
 function to_tone_values(events) {
-	new_map = []
-	events.forEach(function (event) {
-		new_time = event['time'] * Tone.Time('4n')
-		new_event = {'time': new_time, 'note': event['note'], 'duration': event['duration']}
-		new_map.push(new_event);
-	})
-	return new_map
+  return events.map(function(event) { return {time: event.time * Tone.Time('4n'), note: event.note, duration : event.duration} });
 }
-
-// function to_tone_values(events) {
-//   return events.map(function(event) {
-//     { 'time': event.time * Tone.Time('4n'), 'note': event.note, 'duration': event.duration }
-//   })
-// }
